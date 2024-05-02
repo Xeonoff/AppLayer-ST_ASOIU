0788031042c47a51d0b5ca506978776d828372c1
Xeonoff <dayakrut007@mail.ru>
2024-05-02 19:01:53 +0300

    оатмукштг

--- main.js
+++ main.js
@@ -1,35 +1,33 @@
 const express = require('express');
-const bodyParser = require('body-parser'); // Для разбора тела POST запроса
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 const WebSocket = require('ws');
 const axios = require('axios');
-const { v4: uuidv4 } = require('uuid'); // Пакет для генерации UUID
+const { v4: uuidv4 } = require('uuid');
 const wss = new WebSocket.Server({ port: 8080 });
 
 app.use(bodyParser.json());
 
-const connections = {}; // Хранит соотношение между UUID и WebSocket соединениями
-const usersMap = {}; // Хранит соотношение между UUID и именами пользователей
+const connections = {}; 
+const usersMap = {};
 
 wss.on('connection', function connection(ws) {
-  const userId = uuidv4(); // Генерируем уникальный ID для нового пользователя
-  ws.userId = userId; // Сохраняем ID на объекте WebSocket подключения
+  const userId = uuidv4();
 
   ws.on('message', function incoming(message) {
     const data = JSON.parse(message);
 
     if (data.type === 'register') {
       const username = data.username;
-      connections[userId] = ws; // Регистрируем новое соединение
-      usersMap[userId] = username; // Сохраняем имя пользователя
+      connections[userId] = ws; 
+      usersMap[userId] = username;
       return;
     }
 
     if (data.type === 'send') {
-      // Попытка отправить сообщение на основной бэкенд сервер
       const { text, time } = data;
-      const username = usersMap[userId]; // Получаем имя пользователя по его ID
+      const username = usersMap[userId];
       axios.post('http://your-backend-server/send', {
         id: userId,
         username,
@@ -37,7 +35,7 @@ wss.on('connection', function connection(ws) {
         time
       })
       .then(response => {
-        // Обрабатываем успешную отправку
+        response.status();
       })
       .catch(error => {
         console.error('Error sending message:', error);
@@ -53,9 +51,8 @@ wss.on('connection', function connection(ws) {
   });
 
   ws.on('close', () => {
-    // Удаляем пользователя и его соединение при отключении
-    delete connections[ws.userId];
-    delete usersMap[ws.userId];
+    delete connections[userId];
+    delete usersMap[userId];
   });
 });
 app.post('/receive', (req, res) => {
